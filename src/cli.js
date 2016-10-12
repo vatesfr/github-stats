2246b28b1bb56a03f92c637146f91d6f7e459ee6
Julien Fontanet <julien.fontanet@isonoe.net>
2016-10-12 12:01:25 +0200

    Async example: cp

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
 import execPromise from 'exec-promise'
+import { promisify } from 'promise-toolbox'
+import { readFile, writeFile } from 'fs'
+
+const pReadFile = promisify(readFile)
+const pWriteFile = promisify(writeFile)
 
 // ===================================================================
 
-execPromise(args => {
-  throw new Error('not implemented!')
+execPromise(async args => {
+  if (args.length < 2) {
+    throw new Error('not enough args')
+  }
+
+  const [ source, target ] = args
+  const content = await pReadFile(source)
+  await pWriteFile(target, content)
 })
